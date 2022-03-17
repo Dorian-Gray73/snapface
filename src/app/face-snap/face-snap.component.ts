679b871ae92d3f0c155782df7e7889a99b0c7505
Dorian-Gray73 <48696626+Dorian-Gray73@users.noreply.github.com>
2022-03-17 17:48:49 +0100

    creation d'un premier component

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,8 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+export class FaceSnapComponent {}
