2788e1864430c588fe71e91262fbe6729f2ed6aa
Dorian-Gray73 <48696626+Dorian-Gray73@users.noreply.github.com>
2022-03-20 09:42:37 +0100

    creation d'un objet faceSnap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FaceSnap } from './Models/face-snap.models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  mySnap !: FaceSnap;
+
+
+  ngOnInit(){
+    this.mySnap = new FaceSnap(
+    'Lena Söderberg',
+    'La fille qu on voit partout en informatique ',
+    'https://tinyurl.com/34eat443',
+    new Date(),
+    4
+    );
+  }
+}
--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FaceSnap } from '../Models/face-snap.models';
 
 @Component({
   selector: 'app-face-snap',
@@ -6,28 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./face-snap.component.scss']
 })
 export class FaceSnapComponent implements OnInit {
-  title!: string;
-  description! : string;
-  createdDate!: Date;
-  snaps!: number;
-  imageUrl !: string;
+
+  @Input() faceSnap !: FaceSnap;
+
   btnTxt !: string;
 
   ngOnInit(): void {
-      this.title = 'Lena Söderberg';
-      this.description = 'La fille qu on voit partout en informatique ';
-      this.createdDate = new Date();
-      this.snaps = 6;
-      this.imageUrl = 'https://tinyurl.com/34eat443';
       this.btnTxt = 'Oh SNAP';
   }
 
   onAddSnap(){
     if (this.btnTxt == 'Oh SNAP' ) {
-      this.snaps++;
+      this.faceSnap.snaps++;
       this.btnTxt = 'snapped ';
     } else {
-      this.snaps--;
+      this.faceSnap.snaps--;
       this.btnTxt = 'Oh SNAP';
     }
   }
